f3cde5f074c8b3a2c25a1a180f158113a1e5a1d2
Aaron Rietschlin <nemisis.82@gmail.com>
2013-03-19 10:41:18 -0400

    Added comments.

--- src/com/asa/devops/DevOpsDashClockExtension.java
+++ src/com/asa/devops/DevOpsDashClockExtension.java
@@ -6,13 +6,17 @@ import android.provider.Settings;
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
 
+/**
+ * The DashClock extension.
+ */
 public class DevOpsDashClockExtension extends DashClockExtension {
 
 	@Override
 	protected void onUpdateData(int reason) {
-		// Set up the click intent.
+		// Set up the click intent that will take you to the Dev settings
 		Intent clickIntent = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
 
+		// Set up the DashClock extensions data.
 		ExtensionData extensionData = new ExtensionData();
 		extensionData.visible(true).status(getString(R.string.dashclock_extension_status)).icon(R.drawable.ic_launcher);
 		extensionData.expandedTitle(getString(R.string.dashclock_extension_expanded_title));
